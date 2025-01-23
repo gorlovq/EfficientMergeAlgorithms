fe0807a2e45d6b185f95aa2e57c51d21afa5c635
Sergei Gorlov <71068953+gorlovq@users.noreply.github.com>
2025-01-23 22:05:11 +0300

    Framework implementation (#23)
    * init
    
    * impl framework

--- src/framework/framework.cpp
+++ src/framework/framework.cpp
@@ -1,19 +1,13 @@
 #include "framework.hpp"
-#include <vector>
+#include <algorithm>
 #include <ctime>
 #include <cstdlib>
-#include <algorithm>
+#include <vector>
+#include <iostream>
 
-enum class CornerCaseType {
-    RANDOM,                 // Случайные отсортированные массивы
-    FIRST_ALL_SMALLER,      // Все элементы первого массива меньше, чем элементы второго
-    FIRST_ALL_GREATER,      // Все элементы первого массива больше, чем элементы второго
-    PARTIAL_OVERLAP,        // Частичное пересечение диапазонов
-    ONE_ELEMENT_EACH,       // В каждом массиве ровно один элемент
-    EQUAL_ARRAYS,           // Оба массива идентичны
-    DUPLICATES_IN_BOTH,     // В обоих массивах много дубликатов
-    ONE_ARRAY_EMPTY         // Один из массивов пустой
-};
+inline int rand_in_range(int minVal, int maxVal) {
+    return minVal + std::rand() % (maxVal - minVal + 1);
+}
 
 void generate_sorted_vectors(
     int size_a,
@@ -28,20 +22,8 @@ void generate_sorted_vectors(
     a.resize(size_a);
     b.resize(size_b);
 
-    auto rand_in_range = [&](int minVal, int maxVal){
-        return minVal + std::rand() % (maxVal - minVal + 1);
-    };
-
-    for (int i = 0; i < size_a; ++i) {
-        a[i] = rand() % 10000;
-    }
-
-    for (int i = 0; i < size_b; ++i) {
-        b[i] = rand() % 10000;
-    }
-
     switch (caseType) {
-    case CornerCaseType::RANDOM: 
+    case CornerCaseType::RANDOM:
         {
             for (int i = 0; i < size_a; ++i) {
                 a[i] = std::rand() % 10000;
@@ -62,6 +44,7 @@ void generate_sorted_vectors(
             }
         }
         break;
+
     case CornerCaseType::FIRST_ALL_GREATER:
         {
             for (int i = 0; i < size_a; ++i) {
@@ -71,6 +54,69 @@ void generate_sorted_vectors(
                 b[i] = rand_in_range(0, 5000);
             }
         }
+        break;
+
+    case CornerCaseType::PARTIAL_OVERLAP:
+        {
+            // Example: A in [0..5000], B in [3000..8000].
+            for (int i = 0; i < size_a; ++i) {
+                a[i] = rand_in_range(0, 5000);
+            }
+            for (int i = 0; i < size_b; ++i) {
+                b[i] = rand_in_range(3000, 8000);
+            }
+        }
+        break;
+
+    case CornerCaseType::ONE_ELEMENT_EACH:
+        {
+            a.resize(1);
+            b.resize(1);
+
+            a[0] = std::rand() % 10000;
+            b[0] = std::rand() % 10000;
+            size_a = 1;
+            size_b = 1;
+        }
+        break;
+
+    case CornerCaseType::EQUAL_ARRAYS:
+        {
+            if (size_a != size_b) {
+                throw std::runtime_error("EQUAL_ARRAYS corner case requires the same sizes for A and B.");
+            }
+
+            for (int i = 0; i < size_a; ++i) {
+                int val = std::rand() % 10000;
+                a[i] = val;
+                b[i] = val;
+            }
+        }
+        break;
+
+    case CornerCaseType::DUPLICATES_IN_BOTH:
+        {
+            for (int i = 0; i < size_a; ++i) {
+                a[i] = rand_in_range(0, 5);
+            }
+            for (int i = 0; i < size_b; ++i) {
+                b[i] = rand_in_range(0, 5);
+            }
+        }
+        break;
+
+    case CornerCaseType::ONE_ARRAY_EMPTY:
+        {
+            a.clear();
+            a.shrink_to_fit(); // ensure capacity is zero if you'd like
+            size_a = 0;
+
+            // b is random
+            for (int i = 0; i < size_b; ++i) {
+                b[i] = std::rand() % 10000;
+            }
+        }
+        break;
     }
 
     std::sort(a.begin(), a.end());
@@ -78,5 +124,4 @@ void generate_sorted_vectors(
 
     result.resize(size_a + size_b);
     std::merge(a.begin(), a.end(), b.begin(), b.end(), result.begin());
-    std::sort(result.begin(), result.end());
 }
